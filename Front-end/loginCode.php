cbf980ffa4936b8bf158b30c3cce551709f7f5c6
algomez <algomez@iastate.edu>
2015-03-01 15:32:40 -0600

    After trying to figure it out for like 30 minutes, finally fixed the query...

--- Front-end/loginCode.php
+++ Front-end/loginCode.php
@@ -2,23 +2,36 @@
 
 session_start();
 
+//Information for SQL Server. Stored in variables for clarity.
 $server = "mysql.cs.iastate.edu";
 $serverUser = "u30914";
 $serverPassword = "AfzMyGF4c7";
 $serverDatabase = "db30914";
 
+//Blank error message.
 $error = '';
+
+//If stuff has been submitted..
 if($isset($_POST['submit'])) {
+
+    //If either field is empty...
     if (empty($_POST['inputEmail']) || empty($_POST['inputPassword'])) {
         $error = "Email or password is invalid";
     } else {
         $email = $_POST['inputEmail'];
+        $email = "algomez@iastate.edu";
+        $email = trim($email, "@.");
         $password = $_POST['inputPassword'];
+
+        //Open connection to SQL Server.
         $connection = mysql_connect($server, $serverUser, $serverPassword);
 
-        $queryString = "SELECT User_ID, password,
-          FROM User
-          WHERE User_ID=$email AND password=$password";
+        //SQL Query. Typed out here for clarity.
+        $queryString = "SELECT User_ID, password
+                        FROM User
+                        WHERE User_ID=";
+        $queryString .= $email . "AND password=" . $password;
+
 
         $query = mysql_query($queryString);
 

2b92d2eda349a4991ca7f353ba9f2827872bd6cd
algomez <algomez@iastate.edu>
2015-03-05 10:02:27 -0600

    Made changes to search code.

--- backend/search_in_test.php
+++ backend/search_in_test.php
@@ -1,18 +1,23 @@
 <?php
-msql_connect("", "","") or die("could not connect");
-mysql_select_db("") or die("could not connect db");
+
+session_start();
+$server = "mysql.cs.iastate.edu:3306";
+$serverUser = "u30914";
+$serverPassword = "AfzMyGF4c7";
+$serverDatabase = "db30914";
+$connection = msqli_connect($server, $serverUser, $serverPassword, $serverDatabase) or die("Could not connect.");
 $output="";
 if(isset($_POST['search'])){
-    $searchq=$_POST['search'];
-    $searchq=preg_replace("#[^0-9a-z]#i"," ",searchq);
-    $query=mysql_query("SELECT * FROM Activity WHERE content like '%$searchq%'") 
-        or die("could not find search");
+    $search=$_POST['search'];
+    $search=preg_replace("#[^0-9a-z]#i"," ",search);
+    $query = mysqli_query($connection, "SELECT * FROM Activity WHERE content like '%$search%'")
+        or die("Could not find search.");
     $count=mysql_num_rows($query);
     if($count == 0){
-        $output='No result find';
+        $output='No result found.';
     }
     else{
-        while($row=myseql_fetch_arry($query)){
+        while($row=mysqli_fetch_arry($query)){
             $activityID=$row['activityID'];
             $content=$row['content'];
             $output .='<div>'.$activityID. ' '.$content.'</div>';
@@ -21,6 +26,6 @@ if(isset($_POST['search'])){
     }
 }
     
-print("$output");
+print_r("$output");
 
 ?>

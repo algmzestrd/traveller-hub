74d2a81e13d25fd71ffbe8a146065beb7bf0843d
algomez <algomez@iastate.edu>
2015-03-30 14:51:49 -0500

    Fixed typo.

--- Traveler_Hub_Project/postingCode.php
+++ Traveler_Hub_Project/postingCode.php
@@ -12,6 +12,9 @@ $error = '';
         $title = $_POST['title'];
         $description = $_POST['description'];
         $id = $_POST['id'];
+        $location = $_POST['location'];
+        $limit = $_POST['limit'];
+        $type = $_POST['type'];
         $user = $_SESSION['user'];
         $time = $_POST['time'];
         $object = new DateTime("now");
@@ -19,29 +22,44 @@ $error = '';
 
         //Open connection to SQL Server.
         $connection = mysqli_connect($server, $serverUser, $serverPassword, $serverDatabase);
+if($type == "activity") {
+    //SQL Query. Typed out here for clarity.
+    $queryString = "INSERT INTO Activity (Title, Content, User_ID, Activity_ID, Participants, Post_Time, Post_Date, Participant_Limit, Location) VALUES (";
+    $queryString .= "'" . $title . "'" . ", " . "'" . $description . "'" . ", " . "'" . $user . "'" . ", " . "'" . $id . "'" . ", " . "'" . 1 . "'" . ", " . "'" . $time . "'";
+    $queryString .= ", " . "'" . $date . "'" . ", " . "'" . $limit . "'" . ", " . "'" . $location . "')";
 
-        //SQL Query. Typed out here for clarity.
-        $queryString = "INSERT INTO Activity (Title, Content, User_ID, Activity_ID, Participants, Post_Time, Post_Date) VALUES (";
-        $queryString .= "'" . $title . "'" . ", " . "'" . $description . "'" . ", " . "'" . $user . "'" . ", " . "'" . $id . "'" . ", " . "'" . 1 . "'". ", " . "'" . $time . "'";
-        $queryString .= ", " . "'" . $date . "'" . ")";
+
+    $query = mysqli_query($connection, $queryString);
+
+    if (!$query) {
+        $error = var_dump($queryString);
+        //$error = "Please try again. No special characters or duplicate events!";
+    } else {
+        $error = "success";
+    }
+
+    $queryString = "INSERT INTO Participate (Activity_ID, User_ID, Join_Time, Join_Date) VALUES (";
+    $queryString .= "'" . $id . "'" . ", " . "'" . $user . "'" . ", " . "'" . $time . "'";
+    $queryString .= ", " . "'" . $date . "')";
+
+    $query = mysqli_query($connection, $queryString);
+}
+else{
+    $queryString = "INSERT INTO Post (Post_ID, Post_Time, Title, Content, User_ID, Post_Date, Post_Type) VALUES (";
+    $queryString .= "'" . $id . "'" . ", " . "'" . $time . "'" . ", " . "'" . $title . "'" . ", " . "'" . $content . "'" . ", " . "'" . $user . "'" . ", " . "'" . $date . "'";
+    $queryString .= ", " . "'" . $type . "')";
 
 
-        $query = mysqli_query($connection, $queryString);
+    $query = mysqli_query($connection, $queryString);
 
-        if(!$query)
-        {
-            $error = var_dump($queryString);
-            //$error = "Please try again. No special characters or duplicate events!";
-        }
-        else{
-            $error = "success";
-        }
+    if (!$query) {
+        $error = var_dump($queryString);
+        //$error = "Please try again. No special characters or duplicate events!";
+    } else {
+        $error = "success";
+    }
 
-        $queryString = "INSERT INTO Participate (Activity_ID, User_ID, Join_Time, Join_Date) VALUES (";
-        $queryString .= "'" . $id . "'" . ", " . "'" . $user . "'" . ", " . "'" . $time . "'";
-        $queryString .= ", " . "'" . $date . "')";
-
-        $query = mysqli_query($connection, $queryString);
+}
 
         echo $error;
 

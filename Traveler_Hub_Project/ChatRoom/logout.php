25ceaa9ecf486c13c701957fb54b356321ac7906
algomez <algomez@iastate.edu>
2015-04-02 14:47:42 -0500

    Added logout to chat.

---
+++ Traveler_Hub_Project/ChatRoom/logout.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alberto
+ * Date: 3/5/15
+ * Time: 8:38 PM
+ */
+session_start();
+if(!isset($_SESSION['user']))
+{
+    header("Location: loginPage.html");
+}
+session_destroy(); // Destroying All Sessions
+header("Location: loginPage.html"); // Redirecting To Home Page

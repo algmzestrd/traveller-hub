6a4b1b6bb1e5636ef7f8d454bd27319e9c82bdeb
algomez <algomez@iastate.edu>
2015-04-02 15:40:44 -0500

    Edited code for readability.

--- Traveler_Hub_Project/register.php
+++ Traveler_Hub_Project/register.php
@@ -1,52 +1,46 @@
 <?php
 
-session_start();
-
-//Information for SQL Server. Stored in variables for clarity.
-$server = "mysql.cs.iastate.edu:3306";
-$serverUser = "u30914";
-$serverPassword = "AfzMyGF4c7";
-$serverDatabase = "db30914";
-$connection = mysqli_connect($server, $serverUser, $serverPassword, $serverDatabase);
-$object = new DateTime("now");
-$date = $object->format("m-d-Y");
-$role = "User";
-/*
-$options = [
-    'cost' => 11,
-    'salt' => mcrypt_create_iv(22, MCRYPT_DEV_URANDOM),
-];*/
+    session_start();
 
 
-        $email = $_POST['inputEmail'];
-        $password = $_POST['inputPassword'];
-        $passwordLength = strlen($password);
-                //Open connection to SQL Server.
-                $connection = mysqli_connect($server, $serverUser, $serverPassword, $serverDatabase);
+    $server = "mysql.cs.iastate.edu:3306";
+    $serverUser = "u30914";
+    $serverPassword = "AfzMyGF4c7";
+    $serverDatabase = "db30914";
+    $connection = mysqli_connect($server, $serverUser, $serverPassword, $serverDatabase);
+    $object = new DateTime("now");
+    $date = $object->format("m-d-Y");
 
-                //SQL Query. Typed out here for clarity.
-                $email = mysqli_real_escape_string($connection, $email);
-                $password = password_hash($password, PASSWORD_BCRYPT);
-                $queryString = "INSERT INTO User (User_ID , password, register_date, role) VALUES (";
-                $queryString .= "'" . $email . "'" . ", " . "'" . $password . "'" . ", " . "'" . $date . "'" . ", " . "'" . $role . "'" . ")";
+    $email = $_POST['inputEmail'];
+    $password = $_POST['inputPassword'];
+    $role = "User";
 
-                $query = mysqli_query($connection, $queryString);
+    $passwordLength = strlen($password);
 
-                $queryString = "INSERT INTO Profile (User_ID) VALUES(" . "'" . $email . "')";
+    $connection = mysqli_connect($server, $serverUser, $serverPassword, $serverDatabase);
 
-                $query = mysqli_query($connection, $queryString);
+    $email = mysqli_real_escape_string($connection, $email);
+    $password = password_hash($password, PASSWORD_BCRYPT);
 
-                if(!$query)
-{
-$error = "Email or password is already taken.";
+    $queryString = "INSERT INTO User (User_ID , password, register_date, role) VALUES (";
+    $queryString .= "'" . $email . "'" . ", " . "'" . $password . "'" . ", " . "'" . $date . "'" . ", " . "'" . $role . "'" . ")";
+
+    $query = mysqli_query($connection, $queryString);
+
+    $queryString = "INSERT INTO Profile (User_ID) VALUES(" . "'" . $email . "')";
+
+    $query = mysqli_query($connection, $queryString);
+
+    if(!$query) {
+    $error = "Email or password is already taken.";
 }
-else{
-$error = "Registration successful!";
-                 }
-                mysqli_close($connection);
+    else {
+    $error = "Registration successful!";
+}
+    mysqli_close($connection);
 
 
-echo $error;
+    echo $error;
 
 
 

c014cb5f950f84d88704f7c6179c7238c5e352b4
algomez <algomez@iastate.edu>
2015-04-02 15:36:07 -0500

    Edited code for readability.

--- Traveler_Hub_Project/loginCode.php
+++ Traveler_Hub_Project/loginCode.php
@@ -1,22 +1,20 @@
 <?php
 
-session_start();
+    session_start();
 
-//Information for SQL Server. Stored in variables for clarity.
-$server = "mysql.cs.iastate.edu:3306";
-$serverUser = "u30914";
-$serverPassword = "AfzMyGF4c7";
-$serverDatabase = "db30914";
 
-//Blank error message.
+    $server = "mysql.cs.iastate.edu:3306";
+    $serverUser = "u30914";
+    $serverPassword = "AfzMyGF4c7";
+    $serverDatabase = "db30914";
+
     $email = $_POST['inputEmail'];
     $password = $_POST['inputPassword'];
 
-    //Open connection to SQL Server.
     $connection = mysqli_connect($server, $serverUser, $serverPassword, $serverDatabase);
 
-    //SQL Query. Typed out here for clarity.
     $email = mysqli_real_escape_string($connection, $email);
+
     $queryString = "SELECT User_ID, password FROM User WHERE User_ID";
     $queryString .= "=" . "'" . $email . "'";
 
@@ -32,8 +30,7 @@ $serverDatabase = "db30914";
             $response = "success";
             $_SESSION['user'] = $email;
         }
-        else
-        {
+        else {
             $response = "invalid";
         }
     } else {

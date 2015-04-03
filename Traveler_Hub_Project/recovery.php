adbc61a455aa7fdbd9df65b17eaa4228656c7c95
algomez <algomez@iastate.edu>
2015-04-02 20:41:07 -0500

    Started work on password recovery feature.

--- Traveler_Hub_Project/recovery.php
+++ Traveler_Hub_Project/recovery.php
@@ -5,3 +5,14 @@
  * Date: 4/2/15
  * Time: 8:39 PM
  */
+
+session_start();
+
+$to      = $_POST['email'];
+$subject = 'Traveler Hub Password Reset Request';
+$message = 'Hello, You have requested a new password.';
+$headers = 'From: thetravelerhubteam@gmail.com' . "\r\n" .
+    'Reply-To: thetravelerhubteam@gmail.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);

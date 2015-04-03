bda57b5aecea2856cc44ee373807f2111dc07d04
algomez <algomez@iastate.edu>
2015-04-02 20:47:55 -0500

    Started work on password recovery feature.

--- Traveler_Hub_Project/recover.js
+++ Traveler_Hub_Project/recover.js
@@ -1,15 +1,17 @@
 /**
  * Created by alberto on 4/2/15.
  */
-$.post("recovery.php",{email:"algomez@iastate.edu"}, function(data) {
-    if(data =='success'){
-        alert("Success");
-    } else if(data=='failure'){
-        alert("Failure");
-    } else{
-        alert(data);
-    }
-})
-    .fail(function() {
-        alert("error");
+function Mail() {
+    $.post("recovery.php", {email: "algomez@iastate.edu"}, function (data) {
+        if (data == 'success') {
+            alert("Success");
+        } else if (data == 'failure') {
+            alert("Failure");
+        } else {
+            alert(data);
+        }
     })
+        .fail(function () {
+            alert("error");
+        })
+}

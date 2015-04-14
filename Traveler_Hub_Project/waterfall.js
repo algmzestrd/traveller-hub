4c6b53ec0c9983453a1e81a141dc5661fdf78081
“Junlin <jzhu@iastate.edu>
2015-04-13 23:12:10 -0500

    trying to rebuild the waterfall base on the code implemented by Alberto

--- Traveler_Hub_Project/waterfall.js
+++ Traveler_Hub_Project/waterfall.js
@@ -15,7 +15,8 @@ $(document).ready(function(){
 
 
     $.post("getActivities.php",{}, function(data) {
-        if(data != ""){
+        if(data != "")
+        {
             jsonObj = $.parseJSON(data);
 
             var numberOfPosts = jsonObj.length;
@@ -26,7 +27,6 @@ $(document).ready(function(){
             var user;
             var date;
             var i;
-
             for(i = 0; i < numberOfPosts; i++)
             {
                 title = jsonObj[i][1];
@@ -37,52 +37,107 @@ $(document).ready(function(){
                 user = user.split("@");
                 user = user[0];
                 date = jsonObj[i][4];
-                var string = "Title:" + " " + title + "\n";
-                string += "Description:" + " " + description + "\n";
-                string += "Posted By:" + " " + user + "\n";
-                string += "Who's In:" + " " + numberOfParticipants + "\n";
-                string += "Date:" + " " + date;
-                string += "\n";
-                var div = document.createElement('div');
-                var btn = document.createElement('button');
-                btn.id = id;
-                btn.className = 'Join';
-                btn.onclick = Join;
-                var text = document.createTextNode("Join Event!");
-                btn.appendChild(text);
-                div.className = 'water';
-                div.innerHTML = string;
-                div.appendChild(btn);
-              
-                var edit = document.createElement('input');
-                edit.className = 'Edit';
-                edit.type = "submit";
-                edit.value = "Edit Post!";
-                edit.id = user+"edit";
-                edit.action = "post_editing_page.html";
-                var moreText = document.createTextNode("Edit Post!");
-                edit.appendChild(moreText);
-                div.appendChild(edit);
-                edit.onclick = Edit;
-                div.id = id;
-                div.style.border = '2px solid red';
-                div.style.backgroundColor = 'rgb(255, 255, 255)';
-                var j;
-                for( j = 0; j < numberOfParticipants; j++)
+                function createChild()
                 {
-                    var newImg = document.createElement('img');
-                    newImg.setAttribute("src","Materials/user.png");
-                    newImg.setAttribute("height", "20");
-                    newImg.setAttribute("width","20");
-                    div.appendChild(newImg);
+                    var string = "Title:" + " " + title + "\n";
+                    string += "Description:" + " " + description + "\n";
+                    string += "Posted By:" + " " + user + "\n";
+                    string += "Who's In:" + " " + numberOfParticipants + "\n";
+                    string += "Date:" + " " + date;
+                    string += "\n";
+                    var div = document.createElement('div');
+                    var btn = document.createElement('button');
+                    btn.id = id;
+                    btn.className = 'Join';
+                    btn.onclick = Join;
+                    var text = document.createTextNode("Join Event!");
+                    btn.appendChild(text);
+                    btn.style.border = '1px outset #b37d00' ;
+                    div.className = 'water';
+                    div.innerHTML = string;
+                    div.appendChild(btn);
+                    var edit = document.createElement('input');
+                    edit.className = 'Edit';
+                    edit.type = "submit";
+                    edit.value = "Edit Post!";
+                    edit.id = user+"edit";
+                    edit.action = "post_editing_page.html";
+                    edit.style.border = '1px outset #b37d00' ;
+                    var moreText = document.createTextNode("Edit Post!");
+                    edit.appendChild(moreText);
+                    div.appendChild(edit);
+                    edit.onclick = Edit;
+                    div.id = id;
+                    div.style.border = '1px solid #aaa';
+                    div.style.backgroundColor=  'white';
+                    div.style.height =  '100%';
+                    div.style.bottom = '-2px';
+                    div.style.left = '5px';
+                    div.style.right = '5px';
+                    var j;
+                    for( j = 0; j < numberOfParticipants; j++)
+                    {
+                        var newImg = document.createElement('img');
+                        newImg.setAttribute("src","Materials/user.png");
+                        newImg.setAttribute("height", "20");
+                        newImg.setAttribute("width","20");
+                        div.appendChild(newImg);
+                    }
+                    return div;
                 }
-                $("#warp").append(div);
+                function on(element, type, func) 
+                {
+                    if (element.addEventListener) {
+                    element.addEventListener(type, func, false); //false 表示冒泡
+                    } else if (element.attachEvent) {
+                        element.attachEvent('on' + type, func);
+                    } else {
+                        element['on' + type] = func;
+                    }
+                }
+                function getRowByHeight()
+                {
+                    var row = [this.$('row1'),this.$('row2'),this.$('row3'),this.$('row4')];
+                    var height = [];
+                    for(var i = 0;row[i];i++){
+                        row[i].height = row[i].offsetHeight;
+                        height.push(row[i]);
+                    }
+                  
+                    height.sort(function(a,b){
+                    return a.height - b.height;
+                    })
+                    return height;
+                }
+                function getPageHeight(){
+                    return document.documentElement.scrollHeight || document.body.scrollHeight ;
+                }
+                function getScrollTop(){
+                   return document.documentElement.scrollTop || document.body.scrollTop;
+                }
+                function getClientHeigth(){
+                    return document.documentElement.clientHeight || document.body.clientHeight;
+                }
+                function append()
+                {
+                    var i = 0,rows = this.getRowByHeight(),div,k;
+                    for(;this.data[i];i++)
+                    {
+                        div = this.createChild();
+                       
+                        k = ((i+1)>4)?i%4:i;            
+                        
+                        rows[k].appendChild(div);
+                    }
+                }
+                $("#warp").append(createChild());
                 if(currentUser.split("@")[0] != user)
                 {
                 $("#" +user+"edit").hide();
                 }
             }
-        } else{
+        } 
+        else{
             alert("There was an error loading the page.");
         }
     })

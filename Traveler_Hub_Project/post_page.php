05c7dc36987e87de1f7a6aebff330d30541f60f3
Shawn <shawn@shawns-air.student.iastate.edu>
2015-04-25 14:27:26 -0500

    post

--- Traveler_Hub_Project/post_page.php
+++ Traveler_Hub_Project/post_page.php
@@ -6,7 +6,6 @@ session_start();
     <head>
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-        <script src="scriptpages/ddajaxsidepanel.js"></script>
         <script type="text/javascript" src="waterfall.js"></script>
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
             <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
@@ -21,6 +20,7 @@ session_start();
     });
 });
     </script>
+    <script src="scriptpages/ddajaxsidepanel.js"></script>
                         <style type="text/css">
                 
 

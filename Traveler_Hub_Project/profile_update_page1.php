1b0644277a0faac83ea943d04c01ba8012118b78
“Junlin <jzhu@iastate.edu>
2015-04-28 17:56:15 -0500

    delete sharing buttong and intergrad those in the main profile_update_page

--- Traveler_Hub_Project/profile_update_page1.php
+++ Traveler_Hub_Project/profile_update_page1.php
@@ -30,29 +30,7 @@
     <button type="submit" class="btn btn-default">Submit</button>
     </div>
 </form>
-       <div class="isu-QuickLinks">
-
-  <ul class="isu-QuickLinks-right">
-    <li>
-      <a class="isu-Icon--facebook" href="http://www.facebook.com/IowaStateU" title="Facebook">
-        <i class="fa fa-facebook-square"></i>
-        <span class="wd-u-HiddenVisually"></span>
-      </a>
-    </li>
-    <li>
-      <a class="isu-Icon--twitter" href="http://twitter.com/IowaStateU" title="Twitter">
-        <i class="fa fa-twitter-square"></i>
-        <span class="wd-u-HiddenVisually"></span>
-      </a>
-    </li>
-    <li>
-      <a class="isu-Icon--youtube" href="http://www.youtube.com/user/iowastateu" title="YouTube">
-        <i class="fa fa-youtube-square"></i>
-        <span class="wd-u-HiddenVisually"></span>
-      </a>
-    </li>
-  </ul>
-</div>        
+         
         </div>
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
